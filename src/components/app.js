5bd82a14878fa14ee1da3e91f628a1b26b243661
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-04-26 09:36:27 -0500

    scaffolding

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyle from '../styles/global-styles';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello World</h1>
+      <GlobalStyle />
+    </div>
+  );
+}
+
+export default App;
